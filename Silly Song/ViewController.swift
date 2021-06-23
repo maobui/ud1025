5a0591dd272c560c06aee2207c06b73bedc1d936
m.bui <mao.buidinh@hotmail.com>
2021-06-23 22:51:14 +0700

    feat: first app.

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameField.delegate = self
     }
 
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        let text = nameField.text
+        if !(text?.isEmpty ?? true) {
+            lyricsView.text = text
+        }
+    }
 }
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
 
